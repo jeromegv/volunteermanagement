6c56df3f5700cefcf88a5fa1456f13d9fbb2aeaf
Jérôme Gagnon-Voyer <jerome@gagnonvoyer.com>
2014-03-08 15:48:21 -0500

    getting rid of lots of API we don't need

--- config/passport.js
+++ config/passport.js
@@ -6,8 +6,8 @@ var TwitterStrategy = require('passport-twitter').Strategy;
 var GitHubStrategy = require('passport-github').Strategy;
 var GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
 var LinkedInStrategy = require('passport-linkedin-oauth2').Strategy;
-var OAuthStrategy = require('passport-oauth').OAuthStrategy; // Tumblr
-var OAuth2Strategy = require('passport-oauth').OAuth2Strategy; // Venmo, Foursquare
+//var OAuthStrategy = require('passport-oauth').OAuthStrategy; // Tumblr
+//var OAuth2Strategy = require('passport-oauth').OAuth2Strategy; // Venmo, Foursquare
 var User = require('../models/User');
 var secrets = require('./secrets');
 
@@ -298,75 +298,6 @@ passport.use(new LinkedInStrategy(secrets.linkedin, function(req, accessToken, r
   }
 }));
 
-/**
- * Tumblr API
- * Uses OAuth 1.0a Strategy.
- */
-
-passport.use('tumblr', new OAuthStrategy({
-    requestTokenURL: 'http://www.tumblr.com/oauth/request_token',
-    accessTokenURL: 'http://www.tumblr.com/oauth/access_token',
-    userAuthorizationURL: 'http://www.tumblr.com/oauth/authorize',
-    consumerKey: secrets.tumblr.consumerKey,
-    consumerSecret: secrets.tumblr.consumerSecret,
-    callbackURL: secrets.tumblr.callbackURL,
-    passReqToCallback: true
-  },
-  function(req, token, tokenSecret, profile, done) {
-    User.findById(req.user._id, function(err, user) {
-      user.tokens.push({ kind: 'tumblr', accessToken: token, tokenSecret: tokenSecret });
-      user.save(function(err) {
-        done(err, user);
-      });
-    });
-  }
-));
-
-/**
- * Foursquare API
- * Uses OAuth 2.0 Strategy.
- */
-
-passport.use('foursquare', new OAuth2Strategy({
-    authorizationURL: 'https://foursquare.com/oauth2/authorize',
-    tokenURL: 'https://foursquare.com/oauth2/access_token',
-    clientID: secrets.foursquare.clientId,
-    clientSecret: secrets.foursquare.clientSecret,
-    callbackURL: secrets.foursquare.redirectUrl,
-    passReqToCallback: true
-  },
-  function(req, accessToken, refreshToken, profile, done) {
-    User.findById(req.user._id, function(err, user) {
-      user.tokens.push({ kind: 'foursquare', accessToken: accessToken });
-      user.save(function(err) {
-        done(err, user);
-      });
-    });
-  }
-));
-
-/**
- * Venmo API
- * Uses OAuth 2.0 Strategy.
- */
-
-passport.use('venmo', new OAuth2Strategy({
-    authorizationURL: 'https://api.venmo.com/v1/oauth/authorize',
-    tokenURL: 'https://api.venmo.com/v1/oauth/access_token',
-    clientID: secrets.venmo.clientId,
-    clientSecret: secrets.venmo.clientSecret,
-    callbackURL: secrets.venmo.redirectUrl,
-    passReqToCallback: true
-  },
-  function(req, accessToken, refreshToken, profile, done) {
-    User.findById(req.user._id, function(err, user) {
-      user.tokens.push({ kind: 'venmo', accessToken: accessToken });
-      user.save(function(err) {
-        done(err, user);
-      });
-    });
-  }
-));
 
 /**
  * Login Required middleware.
--- config/secrets.js
+++ config/secrets.js
@@ -21,15 +21,6 @@ module.exports = {
     password: 'Your Gmail Password'
   },
 
-  nyt: {
-    key: 'Your New York Times API Key'
-  },
-
-  lastfm: {
-    api_key: 'Your API Key',
-    secret: 'Your API Secret'
-  },
-
   facebookAuth: false,
   facebook: {
     clientID: 'Your App ID',
@@ -69,44 +60,5 @@ module.exports = {
     callbackURL: '/auth/linkedin/callback',
     scope: ['r_fullprofile', 'r_emailaddress', 'r_network'],
     passReqToCallback: true
-  },
-
-  steam: {
-    apiKey: 'Your Steam API Key'
-  },
-
-  twilio: {
-    sid: 'Your Twilio SID',
-    token: 'Your Twilio token'
-  },
-
-  clockwork: {
-    apiKey: 'Your Clockwork SMS API Key'
-  },
-
-  tumblr: {
-    consumerKey: 'Your Consumer Key',
-    consumerSecret: 'Your Consumer Secret',
-    callbackURL: '/auth/tumblr/callback'
-  },
-
-  foursquare: {
-    clientId: 'Your Client ID',
-    clientSecret: 'Your Client Secret',
-    redirectUrl: 'http://localhost:3000/auth/foursquare/callback'
-  },
-
-  venmo: {
-    clientId: 'Your Venmo Client ID',
-    clientSecret: 'Your Venmo Client Secret',
-    redirectUrl: 'http://localhost:3000/auth/venmo/callback'
-  },
-
-  paypal: {
-    host: 'api.sandbox.paypal.com', // or api.paypal.com
-    client_id: 'Your Client ID',
-    client_secret: 'Your Client Secret',
-    returnUrl: 'http://localhost:3000/api/paypal/success',
-    cancelUrl: 'http://localhost:3000/api/paypal/cancel'
   }
 };
