d7bc63bee716acc68233a788d8823967fab45916
Jérôme Gagnon-Voyer <jerome@gagnonvoyer.com>
2014-03-28 22:03:31 -0400

    make express remember last page requested (returnTo)

--- app.js
+++ app.js
@@ -109,8 +109,17 @@ app.use(function(req, res, next) {
   next();
 });
 app.use(flash());
-app.use(app.router);
+//this will cache the static content for a week
 app.use(express.static(path.join(__dirname, 'public'), { maxAge: week }));
+app.use(function(req, res, next) {
+  // Keep track of previous URL
+  if (req.method !== 'GET') return next();
+    var path = req.path.split('/')[1];
+    if (/(auth|login|logout|signup)$/i.test(path)) return next();
+      req.session.returnTo = req.path;
+      next();
+    });
+app.use(app.router);
 app.use(function(req, res) {
   res.status(404);
   res.render('404');
@@ -137,22 +146,31 @@ fs.readdirSync('./controllers').forEach(function (file) {
  */
 
 app.get('/auth/facebook', passport.authenticate('facebook', { scope: ['email', 'user_location'] }));
-app.get('/auth/facebook/callback', passport.authenticate('facebook', { successRedirect: '/', failureRedirect: '/login' }));
+app.get('/auth/facebook/callback', passport.authenticate('facebook', { failureRedirect: '/login' }), function(req, res) {
+  res.redirect(req.session.returnTo || '/');
+});
 app.get('/auth/github', passport.authenticate('github'));
-app.get('/auth/github/callback', passport.authenticate('github', { successRedirect: '/', failureRedirect: '/login' }));
+app.get('/auth/github/callback', passport.authenticate('github', { failureRedirect: '/login' }), function(req, res) {
+  res.redirect(req.session.returnTo || '/');
+});
 app.get('/auth/google', passport.authenticate('google', { scope: 'profile email' }));
-app.get('/auth/google/callback', passport.authenticate('google', { successRedirect: '/', failureRedirect: '/login' }));
+app.get('/auth/google/callback', passport.authenticate('google', { failureRedirect: '/login' }), function(req, res) {
+  res.redirect(req.session.returnTo || '/');
+});
 app.get('/auth/twitter', passport.authenticate('twitter'));
-app.get('/auth/twitter/callback', passport.authenticate('twitter', { successRedirect: '/', failureRedirect: '/login' }));
+app.get('/auth/twitter/callback', passport.authenticate('twitter', { failureRedirect: '/login' }), function(req, res) {
+  res.redirect(req.session.returnTo || '/');
+});
 app.get('/auth/linkedin', passport.authenticate('linkedin', { state: 'SOME STATE' }));
-app.get('/auth/linkedin/callback', passport.authenticate('linkedin', { successRedirect: '/', failureRedirect: '/login' }));
-
+app.get('/auth/linkedin/callback', passport.authenticate('linkedin', { failureRedirect: '/login' }), function(req, res) {
+  res.redirect(req.session.returnTo || '/');
+});
 /**
  * Start Express server.
  */
 
 app.listen(app.get('port'), function() {
-  console.log("✔ Express server listening on port %d in %s mode", app.get('port'), app.settings.env);
+  console.log("✔ Express server listening on port %d in %s mode", app.get('port'), app.get('env'));
 });
 
 module.exports = app;
