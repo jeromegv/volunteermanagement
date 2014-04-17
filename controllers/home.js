ae9836e59234c0d15c9e778501df9254353cf4cb
Jérôme Gagnon-Voyer <jerome@gagnonvoyer.com>
2014-04-17 00:04:38 -0400

    new home page to show the list of people who applied already

--- controllers/home.js
+++ controllers/home.js
@@ -1,11 +1,28 @@
 module.exports.controller = function(app) {
+	var moment = require('moment');
 /**
  * GET /
  * Home page.
  */
- app.get('/', function(req, res) {
-	res.render('home', {
-	    title: 'Home'
-	  });
+ app.get('/', app.locals.passportConf.isAuthenticated, function(req, res) {
+	app.locals.elasticsearchClient.search({
+              index: 'applications',
+              size: 1000,
+              sort: '_timestamp:desc',
+              fields: 'name,email,_timestamp,position_id,resume_attachment_url'
+            }, function (error, response) {
+              if (!error){
+              	console.log(response.hits.hits);
+              	res.render('home', {
+					 title: 'Home',
+					 results: response.hits.hits,
+					 moment: moment 
+				});
+              } else {
+              	console.log(error);
+              }
+            });
+	   
+	
   });
 }
