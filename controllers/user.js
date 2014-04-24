314e3b1c097f56dcfd21b5544b36fa465f2f3632
Jérôme Gagnon-Voyer <jerome@gagnonvoyer.com>
2014-04-23 22:04:15 -0400

    remove gender user profile information from everything

--- controllers/user.js
+++ controllers/user.js
@@ -136,7 +136,6 @@ module.exports.controller = function(app) {
       if (err) return next(err);
       user.email = req.body.email || '';
       user.profile.name = req.body.name || '';
-      user.profile.gender = req.body.gender || '';
       user.profile.location = req.body.location || '';
       user.profile.website = req.body.website || '';
 
